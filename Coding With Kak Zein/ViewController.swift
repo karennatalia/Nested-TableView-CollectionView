21fe62bdf143b5f186629a4ea71cae40d360d9fa
Karen Natalia <karen.natalia888@gmail.com>
2022-04-21 09:15:03 +0700

    Initial Commit

---
+++ Coding With Kak Zein/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coding With Kak Zein
+//
+//  Created by Karen Natalia on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
